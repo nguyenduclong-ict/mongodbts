f43bd7a82a296784241bf058982a7cf55969cafd
Nguyen Duc Long <nguyenduclong.ict@gmail.com>
2021-09-19 23:58:01 +0700

    update

--- dist/utils.d.ts
+++ dist/utils.d.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import { ConnectOptions } from 'mongoose';
-import { CascadeOptions } from 'schema';
+import { CascadeOptions } from './schema';
 import get from 'lodash/get';
 import set from 'lodash/set';
 import pick from 'lodash/pick';
